a976b4ff823ad68ecb831f9d959dd8d8b597c243
Oozdil <37013255+Oozdil@users.noreply.github.com>
2018-12-21 12:27:15 +0300

    Test Dosyası Eklendi

--- OdevTeslim/Test.cs
+++ OdevTeslim/Test.cs
@@ -1,39 +1,105 @@
-﻿using OdevTeslim.Siniflar;
+﻿using NUnit.Framework;
+using OdevTeslim.Siniflar;
 using OdevTeslim.Veritabani;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace OdevTeslim
 {
-    public partial class Test : Form
+    [TestFixture]
+    public class Test
     {
-        public Test()
+        [Test]
+        public void KullaniciGirisTesti()
         {
-            InitializeComponent();
+            ZimmetApp zimmetApp;
+            zimmetApp = new ZimmetApp();
+            Login login = new Login("oozdil", "123", zimmetApp);
+
+            if (zimmetApp.appUser != null)
+            {
+                Assert.Pass("Giriş Başarılı");
+            }
+            else
+            {
+                Assert.Fail("Giriş Başarısız");
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        [Test]
+        public void KullaniciEklemeTesti()
+        {
+                ZimmetApp zimmetApp;
+                zimmetApp = new ZimmetApp();
+                zimmetApp.appUser = new User() { UserID = 1 };
+                User newUser = new User()
+                {
+                    PerID = 6,
+                    UserName = "test",
+                    PassWord = "123",
+                    RoleID = 3
+                };
+                Insert insertIslemi;
+                insertIslemi = new Insert(zimmetApp.appUser);
+                insertIslemi.User(newUser);
+                Assert.Pass("Kullanıcı Ekleme Başarılı");
+       
+        }
+
+        [Test]
+        public void UrunEklemeTesti()
+        {
+            Insert insertIslemi;
+            Urun urun = new Urun()
+            {
+                Isim = "Test İle Eklenen",
+                KategoriID = 1,
+                MarkaID = 1
+            };
+            insertIslemi = new Insert();
+            insertIslemi.Urun(urun);
+            Assert.Pass("Ürün Ekleme Başarılı");
+        }
+
+        [Test]
+        public void StokEklemeTesti()
+        {
+            Insert insertIslemi;
+            User user = new User() { UserID=1};
+            insertIslemi = new Insert(user);
+            Stock stock = new Stock()
+            {
+                UrunID = 12,
+                Fiyat = "1000",
+                FaturaNo = "Test Fatura",
+                FaturaTarihi = "19.12.2018",
+                TedarikciID = 1,
+                StokDurumID = 1
+
+            };
+            insertIslemi.Stock(stock, 1);
+            Assert.Pass("Stok Ekleme Başarılı");
+        }
+
+        [Test]
+        public void ZimmetAtamaTesti()
         {
             StokHareketi stokHareketi = new StokHareketi()
             {
-                DurumID = 1,
-                HareketTipi = 1,
-                KimdenID = 1,
-                 KimeID=2,
-                StokID = 12,
-                Tarih = "test",
+                DurumID = 2,
+                HareketTipi = 2,
+                KimdenID = 0,
+                KimeID = 1,
+                StokID = 1022,
+                Tarih = "19.12.2018",
                 UserID = 1
             };
-
             Update update = new Update();
-            update.Stock(stokHareketi);
+            int sonKayit = update.Stock(stokHareketi);
+            Assert.Pass(sonKayit.ToString()+" Numaralı Stok Ekleme Başarılı");
         }
     }
 }
